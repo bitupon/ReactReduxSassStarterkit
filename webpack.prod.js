2da68d372fcf0afd43a178d2fd71841607c275fa
Mohammad Sadique Sanjar <sanjar.sadique@gmail.com>
2019-09-11 20:19:48 +0530

    index.html was missing in dist.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production', 
@@ -20,6 +21,12 @@ module.exports = merge(common, {
         new CleanWebpackPlugin(['dist/css','dist/js']),
         new MiniCssExtractPlugin({               
             filename: "css/[name].css"
-        })            
+        }),
+		new HtmlWebpackPlugin({
+            title: 'ReactReduxSassStarterkit',
+            inject: false,
+            template: require('html-webpack-template'),
+            bodyHtmlSnippet :'<main class="main" id="app"></main>'
+        })
     ]
 });
